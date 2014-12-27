644ab468917bcc603249e60b7ba853bc53fd8ba8
Tiffany Qi <tiffany19qi@berkeley.edu>
2014-12-26 20:38:23 -0800

    repertoire, media complete
    just need to add four functions to complete

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-    // $("a").click(function() {
-    //     if $(this).html("audio"),
-    //         $(this).function()
-    // })
-
-
-    $("a").click(function() {
-        $(this).css("transition", "0.7s")
+    $('.inline-popup').magnificPopup({
+      type:'inline',
+      midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
     });
+
+
+    // $("a").click(function() {
+    //     $(this).css("transition", "0.7s")
+    // });
 
 }
