f3236ca0d4740578f2d9f2aa3c4fb7cc3e7ed9ae
Tiffany Qi <tiffany19qi@berkeley.edu>
2014-12-28 20:06:07 -0800

    fixed broken js link, add fade, containers, media

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
-    $('.inline-popup').magnificPopup({
-      type:'inline',
-      midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
-    });
-
-
-    // $("a").click(function() {
-    //     $(this).css("transition", "0.7s")
+    // $('.inline-popup').magnificPopup({
+    //   type:'inline',
+    //   midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
     // });
 
-}
+
+    $("body").css("display", "none");
+    $("body").fadeIn("slow");
+
+    $("a").click(function(event) {
+        event.preventDefault();
+        newLocation = this.href;
+        $("body").fadeOut(700, newPage);
+    });
+
+    function newPage() {
+        window.location = newLocation;
+    }
+
+});
