92b54d9884c54d17e0ce28deb68792e30a75873b
Tiffany Qi <tiffany19qi@berkeley.edu>
2014-12-31 16:00:58 -0800

    finished small UX details, added mp3 popup
    change js file, added logo onto each corner through navbar, added
    links, separated email and name,

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,35 @@
 $(document).ready(function(){
-    // $('.inline-popup').magnificPopup({
-    //   type:'inline',
-    //   midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+
+    // Fading of links. Makes some browsers laggy, so it has been removed
+    // $("body").css("display", "none");
+    // $("body").fadeIn();
+
+    // $("a").click(function(event) {
+    //     event.preventDefault();
+    //     newLocation = this.href;
+    //     $("body").fadeOut(400, newPage);
     // });
+    
+    // MP3 popups in media.html
+    $("a.popup-click").click(function() {
+        var mediaBox = $(this).attr("href");
+        
+        $(mediaBox).fadeIn(300);
+        $(mediaBox).css({ 
+            "margin-top": 0,
+            "margin-left": -100,
+        });
 
-
-    $("body").css("display", "none");
-    $("body").fadeIn();
-
-    $("a").click(function(event) {
-        event.preventDefault();
-        newLocation = this.href;
-        $("body").fadeOut(400, newPage);
+        $("body").append('<div class="popup-overlay"></div>');
+        $(".popup-overlay").fadeIn(300);
+        
+        return false;
     });
-
-    function newPage() {
-        window.location = newLocation;
-    }
-
+    
+    $(".popup-overlay").live("click", function() { 
+      $(".popup-overlay, .popup-window").fadeOut(300, function() {
+        $(".popup-overlay").remove();  
+    }); 
+    return false;
+    });
 });
